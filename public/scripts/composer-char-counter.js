335eb55a44504a206aebf14688932c84bf14f785
Gord Letkeman <rndedg@gmail.com>
2022-08-31 19:30:44 +0000

    Set layout for tweet submit form and all tweets list. Add hover states for icons and recent tweets.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  console.log("Ready");
+  
+  $('#tweet-text').keyup(function() {
+    let charCount = $(this).val().length;
+    let counter = $(this).parent().children('.new-tweet-footer').children('.counter');
+    let remainingChars = 140 - charCount;
+    
+    counter.text(remainingChars);
+    if (remainingChars < 0) {
+      counter.addClass("warning");
+    } else {
+      counter.removeClass("warning");
+    }
+  });
+});
+
